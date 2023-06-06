a8548e2427564c9dc9bd3e63c24891d19b2208c1
kiran <kirankahlon08@gmail.com>
2023-06-06 08:15:47 +0530

    Final version

---
+++ app/src/main/java/com/example/buttonwork/MainActivity3.java
@@ -0,0 +1,18 @@
+package com.example.buttonwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+
+
+    }
+}
